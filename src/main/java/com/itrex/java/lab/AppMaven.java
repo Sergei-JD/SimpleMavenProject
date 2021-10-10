91e73c7e43e47a124bde41ec016947848c8a16ca
Sergei-JD <sergshlyazhko@gmail.com>
2021-10-10 19:45:58 +0300

    Simple Maven Project

---
+++ src/main/java/com/itrex/java/lab/AppMaven.java
@@ -0,0 +1,15 @@
+package com.itrex.java.lab;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppMaven {
+
+    public static void main( String[] args ) {
+        System.out.println("\n==============start============\n");
+        System.out.println( "Hello World From Maven!" );
+        System.out.println("\n===============end=============\n");
+    }
+
+}
